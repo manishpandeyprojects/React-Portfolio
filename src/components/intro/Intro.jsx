60053b5e27fb62dddf9d61d12142c591881f98b7
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2023-02-28 18:13:26 +0530

    MP: Intro part is completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,38 @@
-import './intro.scss';
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-const intro = () => {
+const Intro = () => {
+  const textRef = useRef('');
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  });
+
   return (
-    <div className='intro' id='intro'>
-      
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Manish Pandey</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default intro
+export default Intro;
